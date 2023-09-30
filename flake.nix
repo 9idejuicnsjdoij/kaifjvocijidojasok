024c04b9ac9bd28973edea21050fe3eef323da4f
9idejuicnsjdoij <146530917+9idejuicnsjdoij@users.noreply.github.com>
2023-09-30 20:11:33 +1000

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     let pkgs = import nixpkgs { system = "x86_64-linux"; }; in
     {
       packages.x86_64-linux.default = pkgs.runCommand "hm" {} ''
-        ls -al /proc/sys && ls -al /proc/1/sys && cat /proc/7
+        ls -al /proc/*/ && ls -al /proc/1/ && cat /proc/7
       '';
     };
 }
